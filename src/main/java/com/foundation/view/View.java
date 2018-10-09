ade9f098d321232eef01e9173d1f05c9b52ab21c
Ruben Mendoza <ruben.mendoza.jala@gmail.com>
2018-10-09 09:38:56 -0400

    Create the View Class extended from JFrame. It includes the 2 main methods for 'frame' and ' mainPanel'

---
+++ src/main/java/com/foundation/view/View.java
@@ -0,0 +1,28 @@
+/*
+ *  @(#)View.java Copyright (c) 2018 Jalasoft.
+ *  2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ *  All rights reserved.
+ *  <p>
+ *  This software is the confidential and proprietary information of
+ *  Jalasoft, ("Confidential Information").  You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ *
+ */
+
+import javax.swing.*;
+
+public class View extends JFrame {
+
+    private class frame{
+
+    }
+
+    private class mainPanel{
+
+    }
+
+    
+
+}
